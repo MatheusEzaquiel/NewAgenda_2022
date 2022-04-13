aee1f0d2db81739017a8754f823457b1ba86adbd
Matheus Bezerra <75776281+MatheusEzaquiel@users.noreply.github.com>
2022-04-13 11:36:07 -0300

    commit 2

--- editar.php
+++ editar.php
@@ -99,10 +99,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="dist/lula.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Leandro Costa</a>
+          <a href="#" class="d-block">Matheus B.f</a>
         </div>
       </div>
 
--- editar_usuarios.php
+++ editar_usuarios.php
@@ -102,7 +102,7 @@
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Leandro Costa</a>
+          <a href="#" class="d-block">Matheus B.</a>
         </div>
       </div>
 
